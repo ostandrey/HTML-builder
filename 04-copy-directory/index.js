ddef9993f05df09a8163470d982b293eebf2749e
ostandrey <ost.and.g@gmail.com>
2023-05-10 00:54:37 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirPath = path.join(__dirname);
+
+fs.cp(`${dirPath}\\files`, `${dirPath}\\files-copy`, { recursive: true }, (error) => {
+  if (error) console.error(error);
+});
