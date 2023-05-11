af44f2ea69517c09263aba4266eac4aab4bba714
ostandrey <ost.and.g@gmail.com>
2023-05-11 11:02:06 +0300

    feat: create function copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const dirPath = path.join(__dirname);
 
-fs.cp(`${dirPath}\\files`, `${dirPath}\\files-copy`, { recursive: true }, (error) => {
-  if (error) console.error(error);
-});
+const copyDir = (() => {
+  fs.cp(`${dirPath}\\files`, `${dirPath}\\files-copy`, { recursive: true }, (error) => {
+    if (error) console.error(error);
+  });
+})();
